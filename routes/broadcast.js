1a7df0356e8eab7b51e3beaf8494fba8a639203a
Hugh Culling <hughculling@icloud.com>
2023-11-17 22:47:20 +0000

    changed users page

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET index page. */
+/* GET broadcast page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Live Stream Music" });
+  res.render("broadcast", { title: "Live Stream Music" });
 });
 
 module.exports = router;
