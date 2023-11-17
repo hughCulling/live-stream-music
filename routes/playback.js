2ccb446a60d0187f1704d6d2526d7c60042895a0
Hugh Culling <hughculling@icloud.com>
2023-11-17 23:40:27 +0000

    added playback page

--- routes/playback.js
+++ routes/playback.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET broadcast page. */
+/* GET playback page. */
 router.get("/", function (req, res, next) {
-  res.render("broadcast", { title: "Broadcast | Live Stream Music" });
+  res.render("playback", { title: "Playback | Live Stream Music" });
 });
 
 module.exports = router;
