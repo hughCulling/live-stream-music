401cbf3699a422098be773bf5341f2204c0ae158
Hugh Culling <hughculling@icloud.com>
2023-11-18 02:25:58 +0000

    added playback and footer

---
+++ public/javascripts/playback.js
@@ -0,0 +1,8 @@
+if (IVSPlayer.isPlayerSupported) {
+  const player = IVSPlayer.create();
+  player.attachHTMLVideoElement(document.getElementById("video-player"));
+  player.load(
+    "https://f3a548d10fbe.us-east-1.playback.live-video.net/api/video/v1/us-east-1.117906616901.channel.HGnhmOBxnLCW.m3u8"
+  );
+  player.play();
+}
