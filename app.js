c2d2904cd84d263d19489807549d01f7f38a2647
Hugh Culling <hughculling@icloud.com>
2023-11-20 22:05:26 +0000

    Added sign up

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var broadcastRouter = require("./routes/broadcast");
 var playbackRouter = require("./routes/playback");
+var signupRouter = require("./routes/signup");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/broadcast", broadcastRouter);
 app.use("/playback", playbackRouter);
+app.use("/signup", signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
