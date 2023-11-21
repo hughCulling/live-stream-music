364566f1f2a7b4f133ec26403e70d1e6dfea9361
Hugh Culling <hughculling@icloud.com>
2023-11-21 14:45:09 +0000

    sign up works

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,8 +1,71 @@
 var express = require("express");
 var router = express.Router();
 
+//-------------------------------------------------------------
+// MongoDB
+// Require MongoDB language driver
+const { MongoClient } = require("mongodb");
+const uri = require("../atlas_uri");
+
+const client = new MongoClient(uri);
+const dbname = "live-stream-music";
+const collection_name = "users";
+
+const usersCollection = client.db(dbname).collection(collection_name);
+
+// Connect to the database
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log(
+      `Connected to the ${dbname} database 🌍 \nFull connection string: ${uri}`
+    );
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err}`);
+  }
+};
+
+let userAccount = {
+  name: "Phlorpia",
+  email: "hughculling@gmail.com",
+  password: "pw123",
+};
+
+const signUpUser = async () => {
+  try {
+    await connectToDatabase();
+    let result = await usersCollection.insertOne(userAccount);
+    console.log(`Inserted document: ${result.insertedId}`);
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err}`);
+  } finally {
+    await client.close();
+  }
+};
+// Run the main function
+// main();
+
+//----------------------------------------------------------------
+
 /* GET sign up page. */
 router.get("/", function (req, res, next) {
+  res.render("sign_up", { title: "Sign Up | Live Stream Music" });
+});
+
+/* POST sign up page. */
+router.post("/", function (req, res, next) {
+  let userName = req.body.name;
+  let userEmail = req.body.email;
+  let userPassword = req.body.password;
+
+  userAccount = {
+    name: `${userName}`,
+    email: `${userEmail}`,
+    password: `${userPassword}`,
+  };
+
+  signUpUser();
+
   res.render("sign_up", { title: "Sign Up | Live Stream Music" });
 });
 
